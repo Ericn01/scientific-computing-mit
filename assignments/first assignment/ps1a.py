aea2f6d76edc47498c6c06cb2e57d25f9b185a70
Eric Nielsen <nielseneric2001@gmail.com>
2026-03-09 12:37:00 -0600

    Implemented greedy and brute force algorithms.
    Space Cows assignment is completed.

--- assignments/first assignment/ps1a.py
+++ assignments/first assignment/ps1a.py
@@ -1,8 +1,8 @@
 ###########################
 # 6.0002 Problem Set 1a: Space Cows 
-# Name:
-# Collaborators:
-# Time:
+# Name: Eric Nielsen
+# Collaborators: N/A
+# Time: 3:00PM
 
 from ps1_partition import get_partitions
 import time
@@ -24,8 +24,24 @@ def load_cows(filename):
     Returns:
     a dictionary of cow name (string), weight (int) pairs
     """
-    # TODO: Your code here
-    pass
+    cows = dict() # initialize an empty dictionary
+    try:
+        with open(file=filename, mode="r", encoding="utf-8") as file:
+            for line in file.readlines():
+                name, weight = line.strip().split(",")
+                cows[name] = int(weight)
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' could not be found")
+    except IOError as e:
+        # Catches other I/O related errors (e.g., permission issues)
+        print(f"I/O error: {e}")
+    return cows
+
+cows = load_cows("./assignments/first assignment/ps1_cow_data.txt")
+
+def pretty_print_cows(cows):
+    for i, (name, weight) in enumerate(cows.items()):
+        print(f"[{i + 1}] {name.title()}: {weight}")
 
 # Problem 2
 def greedy_cow_transport(cows,limit=10):
@@ -49,9 +65,27 @@ def greedy_cow_transport(cows,limit=10):
     A list of lists, with each inner list containing the names of cows
     transported on a particular trip and the overall list containing all the
     trips
+
     """
-    # TODO: Your code here
-    pass
+    
+    trips = []
+    remaining = sorted(cows, key=cows.get, reverse=True)
+
+    while remaining:
+        trip, trip_weight = [], 0
+        leftover = []
+        for name in remaining:
+            weight = cows[name]
+            if trip_weight + weight <= limit:
+                trip.append(name)
+                trip_weight += weight 
+            else:
+                leftover.append(name)
+        trips.append(trip)
+        remaining = leftover
+    return trips
+
+trips = greedy_cow_transport(cows)
 
 # Problem 3
 def brute_force_cow_transport(cows,limit=10):
@@ -75,9 +109,37 @@ def brute_force_cow_transport(cows,limit=10):
     transported on a particular trip and the overall list containing all the
     trips
     """
-    # TODO: Your code here
-    pass
+
+    def calc_trip_weight(trip, cows):
+        weight_sum = 0
+        for name in trip:
+            weight = cows[name]
+            weight_sum += weight 
+            
+        return weight_sum
+
+    ps = get_partitions(cows) # generate the power set 
+    trip_dict = dict()
+    success_partitions = dict()
+    for cow_part in ps:
+        valid_part = True
+        for trip in cow_part:
+            str_trip = str(trip)
+            trip_weight = trip_dict.get(str_trip)
+            if trip_weight is None:
+                trip_weight = calc_trip_weight(trip, cows)
+                trip_dict[str_trip] = trip_weight
+            
+            if trip_weight > limit: # no point in searching anymore
+                valid_part = False
+                break
         
+        if valid_part:
+            success_partitions[str(cow_part)] = len(cow_part)
+    
+    shortest_trip = success_partitions[min(success_partitions, key=success_partitions.get)]
+    return shortest_trip
+
 # Problem 4
 def compare_cow_transport_algorithms():
     """
@@ -92,5 +154,20 @@ def compare_cow_transport_algorithms():
     Returns:
     Does not return anything.
     """
-    # TODO: Your code here
-    pass
+
+    def test_func_speed(label, func, cows, limit=10):
+        print(f"\n=== TESTING {label} ===\n")
+        start = time.perf_counter()
+        output = func(cows, limit)
+        end = time.perf_counter()
+        elapsed = end - start
+        print(f"OUTPUT: {output}\nEXECUTION TIME (ms): {elapsed}")
+    
+    labels = ["Greedy Algorithm", "Brute Force Algorithm"]
+    funcs = [greedy_cow_transport, brute_force_cow_transport]
+
+    for i, func in enumerate(funcs):
+        test_func_speed(labels[i], func, cows, limit=20)
+
+
+compare_cow_transport_algorithms()
